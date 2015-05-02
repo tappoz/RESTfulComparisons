93d3423df7e509dc531afa987fb26dbca9b2646e
tappoz <tappoz84@yahoo.it>
2015-05-02 16:31:18 +0100

    React now fetches the JSON from file and shows it

--- frontend-js-implementation/src/reactCode.js
+++ frontend-js-implementation/src/reactCode.js
@@ -1,19 +1,27 @@
-// tutorial1.js
 var BarChartRenderer = React.createClass({
-
-	componentWillMount: function() {
-
-    var jsonFilePath = 'data/backendApiData.json';
-    // TODO jQuery / AJAX to load the content
+  getInitialState: function() {
+    var now = new Date();
+    console.log('Started at:', now.getMilliseconds());
+    return {started: now}; // this.state.started
+  },
+  componentWillMount: function() {
+    $.getJSON("data/backendApiData.json", function (dataFromFileSystem) {
+      console.log(new Date().getMilliseconds(), "This data has been loaded:", dataFromFileSystem);
+      this.setState({dataToShow: dataFromFileSystem});
+    }.bind(this));
   },
   render: function() {
+    console.log(new Date().getMilliseconds(), "The dataToShow we found is:", this.state.dataToShow);
+    var formattedData = JSON.stringify(this.state.dataToShow, null, 2);
     return (
       <div className="barChartRenderer">
-        Hello, world! I am a BarChartRenderer managed by React!
+        Hello, world! I am a BarChartRenderer managed by React! 
+        <tt>{formattedData}</tt>
       </div>
     );
   }
 });
+
 React.render(
   <BarChartRenderer />,
   document.getElementById('reactD3BarChart')
